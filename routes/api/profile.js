2c2eb1b5932f669d84eb7e36f4e4753c75877247
Christian Jensen <chrisnj.ftw@gmail.com>
2018-07-30 23:24:04 +0200

    added profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,13 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Load Profile Model
+const Profile = require('../../models/Profile');
+//Load User Prfile
+const User = require('../../models/Users');
 
 // @route   GEt api/profile/test
 // @des     Test profile route
@@ -9,5 +16,25 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
     msg: 'profile works'
 }));
+
+// @route   GEt api/profile
+// @des     Current user profile route
+// @access  Private
+router.get('/', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const errors = {};
+    Profile.findOne({
+            user: req.user.id
+        })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
